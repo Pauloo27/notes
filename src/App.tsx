719cf3fb507c9322d85eff69b5324ce0ba956aea
Paulo <pauloeduardo2709@gmail.com>
2020-12-28 23:07:10 -0300

    Add home page place holder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import * as React from "react";
+import { 
+  BrowserRouter as Router, Switch, Route, Redirect
+} from "react-router-dom";
+import Home from "./pages/home/index";
+import "./styles/App.css";
 
 export default function App() {
   return (
-    <h1>Hello</h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="*">
+          <Redirect to="/"/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
