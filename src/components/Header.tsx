140a099f2578624fe0536e58dd38746dc812908f
Paulo <pauloeduardo2709@gmail.com>
2020-12-29 18:35:47 -0300

    Add Links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
   return (
     <header>
-      <h1>Notes</h1>
-      <button className="btn-default btn-icon">
-        <FontAwesomeIcon icon={faPlus} color="#f8f8f2" />
-      </button>
+      <Link to="/">
+        <h1>Notes</h1>
+      </Link>
+      <Link to="/new">
+        <button className="btn-default btn-icon">
+          <FontAwesomeIcon icon={faPlus} color="#f8f8f2" />
+        </button>
+      </Link>
     </header>
   );
 }
