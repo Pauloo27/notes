d51cd653ce9a43aca507196f5c4c69d39b817c5d
Paulo <pauloeduardo2709@gmail.com>
2020-12-30 16:36:50 -0300

    Add manifest and empty sw.js

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
+if("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+  .then(() => console.log("Service worker registered"))
+  .catch(() => console.log("Falied to load server worker"))
+} else {
+  console.log("Service worker not supported");
+}
 ReactDOM.render(<App />, document.getElementById("root"));
